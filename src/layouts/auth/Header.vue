5874c84c76cf90c71549af843a7f5404284f576e
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-07-09 13:44:29 +0200

    feat: Add change language in header

--- src/layouts/auth/Header.vue
+++ src/layouts/auth/Header.vue
@@ -1,7 +1,33 @@
 <template>
   <header class='dashboard-header'>
-    <div class="navbar-header">
-      test
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <!-- left contains (toggle sidebar button and search input) -->
+        <div class="left">
+          test
+        </div>
+
+        <!-- right contains (change language, notifications and user actions) -->
+        <div class="right">
+          <div class="dropdown header-item">
+            <button type="button" class="dropdown-toggle-btn" :title="$t('change_language')" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img id="header-lang-img" src="@/assets/images/flags/en.svg" :alt="$t('Languages')" width="20" height="20" class="rounded">
+            </button>
+            <div class="dropdown-menu dropdown-menu-end">
+              <!-- item-->
+              <a href="javascript:void(0);" class="dropdown-item" data-lang="en" :title="$t('english')">
+                <img src="@/assets/images/flags/en.svg" :alt="$t('english')" width="18" height="18" class="rounded">
+                <span>{{ $t('english') }}</span>
+              </a>
+              <!-- item-->
+              <a href="javascript:void(0);" class="dropdown-item" data-lang="en" :title="$t('arabic')">
+                <img src="@/assets/images/flags/ar.svg" :alt="$t('arabic')" width="18" height="18" class="rounded">
+                <span>{{ $t('arabic') }}</span>
+              </a>
+            </div>
+        </div>
+        </div>
+      </div>
     </div>
   </header>
 </template>
@@ -11,6 +37,24 @@ export default {
   name: 'Header',
 };
 </script>
+
+<i18n>
+  {
+    "en": {
+      "Languages": "Languages",
+      "change_language": "Change language",
+      "english": "English",
+      "arabic": "Arabic",
+    },
+
+    "ar": {
+      "Languages": "اللغات",
+      "change_language": "تغيير اللغة",
+      "english": "الإنجليزية",
+      "arabic": "العربية",
+    },
+  }
+</i18n>
 
 <style lang='scss' scoped>
 @import '@/assets/scss/layouts/auth/_header.scss';
